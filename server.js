361272e001eb814a802a9e953dba3defe77c8150
yebaojia <695988558@qq.com>
2021-01-30 18:04:44 +0800

    测试0.0.0.0

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-const port = 8080;
-app.listen(port, err => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('Listening at: http://localhost:' + port);
-});
+// const port = 8080;
+// app.listen(port, err => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('Listening at: http://localhost:' + port);
+// });
+
+
+var http = require('http');
+var ip = '0.0.0.0';
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, ip);
+console.log('Server running at http://'+ip+':1337/');
