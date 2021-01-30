3b7071d4ae957b9b51bf53e31b44254e01fa55c6
yebaojia <695988558@qq.com>
2021-01-30 20:05:34 +0800

    express

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-// const port = 8080;
-// app.listen(port, err => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log('Listening at: http://localhost:' + port);
-// });
+const port = 80;
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at: http://localhost:' + port);
+});
 
 
-var http = require('http');
-var ip = '0.0.0.0';
-var port = 8080;
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port, ip);
-console.log('Server running at http://'+ip + ':' + port);
+// var http = require('http');
+// var ip = '0.0.0.0';
+// var port = 8080;
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(port, ip);
+// console.log('Server running at http://'+ip + ':' + port);
